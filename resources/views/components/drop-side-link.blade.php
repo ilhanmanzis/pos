45718a9dbce4564dd710c8da852b1483ba185262
ilhanmanzis <ilhanmanzis1207@gmail.com>
2025-06-18 11:26:53 +0700

    l

--- resources/views/components/drop-side-link.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['page'])
-
-<li>
-    <a {{ $attributes }} class="menu-dropdown-item group"
-        :class="page === '{{ $page }}' ? 'menu-dropdown-item-active' :
-            'menu-dropdown-item-inactive'">
-        {{ $slot }}
-    </a>
-</li>
